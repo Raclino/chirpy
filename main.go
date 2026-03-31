d6b211d63481386ebbc18d3abe1d794bcdea4c2d
Thomas Daniel <daniel_thomas1@outlook.fr>
2026-04-01 01:09:52 +0200

    Add reset & Metrics handlers + metrics middleware

--- main.go
+++ main.go
@@ -1,21 +1,36 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const filePathRoot = "."
 	const port = "8080"
+	apiConfig := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 	muxServer := http.NewServeMux()
-	muxServer.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filePathRoot))))
-	muxServer.Handle("/assets/logo.png", http.StripPrefix("/assets/logo.png", http.FileServer(http.Dir(filePathRoot))))
+	muxServer.Handle("/app/", apiConfig.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(filePathRoot)))))
 	muxServer.HandleFunc("/healthz", handlerHealth)
+	muxServer.HandleFunc("/metrics", apiConfig.handlerMetrics)
+	muxServer.HandleFunc("/reset", apiConfig.handlerReset)
 
 	server := &http.Server{
 		Handler: muxServer,
 		Addr:    ":" + port,
 	}
+
 	if err := server.ListenAndServe(); err != http.ErrServerClosed {
 		log.Fatal(err)
+	}
 
 }
 
@@ -24,4 +39,22 @@ func handlerHealth(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 
 	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	w.Write([]byte(fmt.Sprintf("Hits: %d\n", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = atomic.Int32{}
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
